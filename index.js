84483828e66b2e025601966bea49b3c8f9e2d510
Fanqiang Meng <fanmeng0417@gmail.com>
2019-06-24 03:21:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,16 +1,2 @@
 // Write your solution here!
-const  append = ["Milo", "Otis", "Garfield", "Odie"];
-
-const  prepend = ["Odie", "Milo", "Otis", "Garfield"];
-
-const  removeLast = ["Odie", "Milo", "Otis", "Garfield"];
-
-removeLast.pop();
-
-removeLast;
-
-const  removeFirst = ["Odie", "Milo", "Otis", "Garfield"];
-
-removeFirst.shift();
-
-removeFirst;
+const pet = []
